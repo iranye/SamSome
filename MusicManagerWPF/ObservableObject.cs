6de20244f2693fad1c1566d16eb8e4caebe826a3
Ira Nye <Ira.Nye@usdigital.com>
2016-02-24 09:22:39 -0800

    Add MusicManager WPF project

---
+++ MusicManagerWPF/ObservableObject.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace MusicManager
+{
+    [Serializable]
+    public abstract class ObservableObject : INotifyPropertyChanged
+    {
+        [field:NonSerialized]
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            var handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        protected void RaisePropertyChanged<T>(Expression<Func<T>> propertyExpression)
+        {
+            var propertyName = PropertySupport.ExtractPropertyName(propertyExpression);
+            this.RaisePropertyChanged(propertyName);
+        }
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            VerifyPropertyName(propertyName);
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Warns the developer if this Object does not have a public property with
+        /// the specified name. This method does not exist in a Release build.
+        /// </summary>
+        [Conditional("DEBUG")]
+        [DebuggerStepThrough]
+        public void VerifyPropertyName(String propertyName)
+        {
+            if (TypeDescriptor.GetProperties(this)[propertyName] == null)
+            {
+                Debug.Fail("Invalid property name: " + propertyName);
+            }
+        }
+    }
+}
