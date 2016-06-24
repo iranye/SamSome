78de0ae531cb2fc7ce729f7445c01863949f8f88
Ira Nye <Ira.Nye@usdigital.com>
2016-06-23 17:12:23 -0700

    Add HackerRank practice code.

---
+++ Practice/HackerRank/DataStructures/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        Console.Write("Enter number of numbers you're entering: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Reading {0} numbers", n);
+        string[] arr_temp = Console.ReadLine().Split(' ');
+
+        int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+        //foreach (var el in arr)
+        //{
+        //    Console.Write("{0} ", el);
+        //}
+    }
+}
