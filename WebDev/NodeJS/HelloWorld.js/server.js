07802bfaa7aaa3695d72d9523541cfe83d0cb2c3
iranye <iranye@gmail.com>
2017-05-12 21:10:54 -0700

    new NodeJS files

--- WebDev/NodeJS/HelloWorld.js/server.js
+++ WebDev/NodeJS/HelloWorld.js/server.js
@@ -1,7 +1,8 @@
 ﻿var http = require('http');
 var port = process.env.port || 1337;
-http.createServer(function (req, res) {
+var server = http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
-}).listen(port);
-console.log("Server running at http://127.0.0.1:" + port);
+});
+server.listen(port);
+console.log("Server running at http://localhost:%s", port);
--- WebDev/NodeJS/main.js
+++ WebDev/NodeJS/main.js
@@ -1,1 +1,3 @@
+"use strict";
+x = 3.14; // this will fail running: "node main" because of "use strict"
 console.log("Hello")
