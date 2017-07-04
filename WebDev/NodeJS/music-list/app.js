d537f112144b9f4c410ec07607a1079aa86096ff
ira nye <iranye@gmail.com>
2017-07-03 17:18:45 -0700

    update music-list, add new typings files

---
+++ WebDev/NodeJS/music-list/app.js
@@ -0,0 +1,21 @@
+﻿'use strict';
+
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow = null;
+console.log("foobar");
+console.log(require('osenv').home());
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow();
+    mainWindow.loadURL(`file://${app.getAppPath()}/index.html`);
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+});
---
+++ WebDev/NodeJS/music-list/music-list_README.txt
@@ -0,0 +1,6 @@
+﻿This project is loosely based on Lorikeet app from Cross-Platform Desktop Applications.
+
+The three parts that comprise the features:
+HTML for GUI
+CSS for layout and styling
+JavaScript for file IO and UI display
