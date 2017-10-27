d5d4a3ef18f2434eb7235ec48fa5cc055bce1c17
iranye <iranye@gmail.com>
2017-10-27 16:00:51 -0700

    add new WPF project add sort functionality to Regexer

---
+++ Practice/CSharpBooks/IllustratedWpfRich/ShowTime.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace IllustratedWpfRich
+{
+    class ShowTime : MarkupExtension
+    {
+        private string mHeader = string.Empty;
+        public string Header
+        {
+            get { return mHeader; }
+            set { mHeader = value; }
+        }
+
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return $"{Header}: {DateTime.Now.ToLongDateString()}";
+        }
+
+        public ShowTime()
+        {
+        }
+
+        public ShowTime(string input)
+        {
+            Header = input;
+        }
+    }
+}
