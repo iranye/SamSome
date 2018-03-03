167f15a89e7848861df68b456df727ef1e4a3f5d
Ira Nye <iranye@gmail.com>
2018-03-03 13:06:32 -0800

    Move code form FavIcons project to AsyncInCsharp5 project

---
+++ Practice/CSharpBooks/AsyncInCsharp5_0/StatusViewModel.cs
@@ -0,0 +1,37 @@
+﻿using MicroMvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncInCsharp5_0
+{
+    class StatusViewModel : ObservableObject
+    {
+        private string mLogMessage = String.Empty;
+        public string LogMessage
+        {
+            get { return mLogMessage; }
+            private set
+            {
+                if (mLogMessage != value)
+                {
+                    mLogMessage = value;
+                    NotifyPropertyChanged("LogMessage");
+                }
+            }
+        }
+
+        public void AddLogMessage(string message)
+        {
+            var rotateOutAfterSoManyLines = "";
+            LogMessage += $"{message} {Environment.NewLine}";
+        }
+
+        public void ClearLog()
+        {
+            LogMessage = String.Empty;
+        }
+    }
+}
