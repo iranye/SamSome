cbb5945577ca398636e1c6ad28a0a4b18923d67f
iranye <iranye@gmail.com>
2018-12-31 14:02:28 -0800

    update Perl

--- Scripting/Perl/QuickSub.pl
+++ Scripting/Perl/QuickSub.pl
@@ -10,11 +10,89 @@ sub main {
     if (defined($opt{'o'})) {
       $overwriteFlag = 1;
     }
-    readAndReplace($opt{'f'}, $overwriteFlag);
+    # readAndReplace($opt{'f'}, $overwriteFlag);
+    # readAndReplaceStringBlocks($opt{'f'});
+    ensureTestIntegrationAttribute($opt{'f'});
   }
   else {
     printf "Usage: %s -f <file to update> [-o]\n", $0;
     printf "   -o: Overwrite file\n";
+  }  
+}
+
+sub ensureTestIntegrationAttribute {  
+  my $file = shift;
+  open(FH, "<$file")||die("Error: Cannot open file $file for input !!!\n\n");
+  
+  my $scanningAttributes = 0;
+  my $integrationAttributeFound = 0;
+  my $i = 0;
+  while(<FH>) {
+    $i++;
+    chomp;
+    if ($scanningAttributes == 1) {
+      if (!/[\s]+\[.*/) { # if we've reached a non-Attribute line and we haven't encountered the requisite Attibute line, add it
+        $scanningAttributes = 0;
+        if ($integrationAttributeFound == 0) {
+          # print '[TestCategory("Integration")]\r\n"' ;
+          printf "        %s\r\n", '[TestCategory("Integration")]';
+        }
+        $integrationAttributeFound = 0;
+      }
+      if (/[\s]+\[TestCategory\("Integration"\)\].*/) {
+        $integrationAttributeFound = 1;
+      }
+    }
+    if (/[\s]+\[TestMethod\].*/) {
+      $scanningAttributes = 1;
+      # if (/[\s]+\[(\S+)\].*/) {
+        # printf "%s\r\n", $1;
+      # }
+    }
+    # printf "[%s] scanningAttributes: %s got int attrib: %s\r\n", $i, $scanningAttributes, $integrationAttributeFound;
+    printf "%s", $_;
+  }  
+}
+
+sub readAndReplaceStringBlocks {
+  my $file = shift;
+  # my $tempFile = "temp.txt";
+  open(FH, "<$file")||die("Error: Cannot open file $file for input !!!\n\n");
+  
+  # open(OFH, ">>$tempFile")||die("Error: Cannot open file $tempFile for output !!!\n\n");
+    
+  # my $replacementStr = "SPROC_NAME";
+  my $codeBlock = '
+
+        [TestMethod]
+        [TestCategory("ZpTransactions")]
+        public void Test_SPROC_NAME()
+        {
+            SprocName = "SPROC_NAME";
+            var sqlParameters = new List<SqlParameter> {
+                new SqlParameter { ParameterName = "@paymentID", Value = 1, SqlDbType = SqlDbType.Int, IsNullable = false }
+            };
+            try
+            {
+                var testRunTimings = RunSprocAsync(sqlParameters).GetAwaiter().GetResult();
+                UpdateTestData(testRunTimings, Util.GetCurrentMethodName());
+            }
+            catch (Exception ex)
+            {
+                UpdateTestData(FailedTestTimings, Util.GetCurrentMethodName());
+                throw;
+            }
+        }
+  ';
+  
+  # printf "%s\n", $codeBlock;
+  
+  while(<FH>) {
+    chomp;
+    (my $sprocName = $_) =~ s/([\S]+)\s*/$1/;
+    # printf "'%s'\r\n", $sprocName;
+    (my $newCode = $codeBlock) =~ s/(.*)SPROC_NAME(.*)/$1$sprocName$2/g;
+    print $newCode;
   }  
 }
 
