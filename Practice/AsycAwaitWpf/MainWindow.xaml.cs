a7c0267ddfed3ec0243d1867ce086c262e33b8ac
iranye <iranye@gmail.com>
2019-04-18 15:15:59 -0700

    add AsyncAwait project (youtube)

---
+++ Practice/AsycAwaitWpf/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AsycAwaitWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ExecuteSync_Click(object sender, RoutedEventArgs e)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            RunDownloadSync();
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            resultsWindow.Text += $"Total execution time (Sync): {elapsedMs}";
+        }
+
+        private void RunDownloadSync()
+        {
+            List<string> websites = PrepData();
+            foreach (var site in websites)
+            {
+                var results = DownloadWebsite(site);
+                ReportWebsiteInfo(results);
+            }
+        }
+
+        private async void ExecuteAsync_Click(object sender, RoutedEventArgs e)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            await RunDownloadAsync(); // RunDownloadParallelAsync(); // RunDownloadAsync();
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            resultsWindow.Text += $"Total execution time (Sync): {elapsedMs}";
+
+        }
+
+        private async Task RunDownloadAsync()
+        {
+            List<string> websites = PrepData();
+            foreach (var site in websites)
+            {
+                var results = await Task.Run(() => DownloadWebsite(site));
+                ReportWebsiteInfo(results);
+            }
+        }
+
+        private async Task RunDownloadParallelAsync()
+        {
+            List<string> websites = PrepData();
+            List<Task<WebsiteDataModel>> tasks = new List<Task<WebsiteDataModel>>();
+            foreach (var site in websites)
+            {
+                tasks.Add(Task.Run(() => DownloadWebsite(site)));
+            }
+
+            WebsiteDataModel[] results = await Task.WhenAll(tasks);
+
+            foreach (var item in results)
+            {
+                ReportWebsiteInfo(item);
+            }
+        }
+
+        private void ReportWebsiteInfo(WebsiteDataModel data)
+        {
+            resultsWindow.Text += $"{data.WebsiteUrl} downloaded: { data.WebsiteData.Length} characters {Environment.NewLine}";
+        }
+
+        private WebsiteDataModel DownloadWebsite(string url)
+        {
+            var output = new WebsiteDataModel();
+            WebClient client = new WebClient();
+
+            output.WebsiteUrl = url;
+            output.WebsiteData = client.DownloadString(url);
+
+            return output;
+        }
+
+        private List<string> PrepData()
+        {
+            var output = new List<string>();
+
+            resultsWindow.Text = "";
+            output.Add("https://www.yahoo.com");
+            output.Add("https://www.google.com");
+            output.Add("https://www.microsoft.com");
+            output.Add("https://www.cnn.com");
+            output.Add("https://www.codeproject.com");
+            output.Add("https://www.stackoverflow.com");
+
+            return output;
+        }
+    }
+}
---
+++ Practice/AsycAwaitWpf/README.txt
@@ -0,0 +1,1 @@
+﻿https://www.youtube.com/watch?v=2moh18sh5p4
