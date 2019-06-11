a6ffde4099109c58ea3a35052379e08e1b21ebca
Ira Nye <iranye@gmail.com>
2019-06-11 15:58:50 -0700

    Add MyNinjas sln

---
+++ Practice/Pluralsight/Csharp/EntityFramework/MySln/NinjaDomain.Classes/Classes.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace NinjaDomain.Classes
+{
+    public enum NinjaType
+    {
+        Shinobi = 1,
+        Kunoichi = 2
+
+    }
+
+    public enum EquipmentType
+    {
+        Tool = 1,
+        Weapon = 2,
+        Outwear = 3
+    }
+
+    public class Ninja
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool ServedInOniwaban { get; set; }
+        public Clan Clan { get; set; }
+        public int ClanId { get; set; }
+        public List<NinjaEquipment> EquipmentOwned { get; set; }
+        public System.DateTime DateOfBirth { get; set; }
+    }
+
+    public class Clan
+    {
+        public int Id { get; set; }
+        public string ClanName { get; set; }
+        public List<Ninja> Ninjas { get; set; }
+    }
+
+    public class NinjaEquipment
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public EquipmentType Type { get; set; }
+        [Required]
+        public Ninja Ninja { get; set; }
+    }
+}
